e7b8027d7ac6dd2618ff0afdf9256c6e5eb5d9bd
LoanDevos <100829637+LoanDevos@users.noreply.github.com>
2022-06-21 15:50:54 +0200

    Update scriptv2.py
    verify components list not empty and verify that there is not more than one "@"

--- scriptv2.py
+++ scriptv2.py
@@ -12,15 +12,16 @@ def vulnerabilites():
     tab = []
     with open("vmoex-framework/bom.json", encoding="utf-8") as bom_datas:
         data = json.load(bom_datas)
-    for k in range(len(data["components"])):
-        for valeur in data["components"][k]:
-            if valeur == "purl":
-                if "@" in data["components"][k][valeur]:
-                    if "?" in data["components"][k][valeur]:
-                        tmp = str(data["components"][k][valeur]).split("?")
-                        tab.append(tmp[0])
-                    else:
-                        tab.append(data["components"][k][valeur])
+    if "components" in data:
+        if data["components"]!=None:
+            for item in data["components"]:
+                if "purl" in item:
+                    if (item["purl"]).count("@")==1:
+                        if "?" in item["purl"]:
+                            tmp = str(item["purl"]).split("?")
+                            tab.append(tmp[0])
+                        else:
+                            tab.append(item["purl"])
     headers = {
         "accept": "application/vnd.ossindex.component-report.v1+json",
         "Content-Type": "application/vnd.ossindex.component-report-request.v1+json",
